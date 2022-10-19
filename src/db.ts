98be9f1e77bd5b60b5287dbab480c36d7d179356
Dennis Keil <dennis.keil10@gmail.com>
2022-10-19 21:04:32 +0200

    update all

--- src/db.ts
+++ src/db.ts
@@ -4,5 +4,6 @@ import {loadEnv} from "./deps.ts";
 await loadEnv();
 
 export const prisma = new PrismaClient({
+    log: ['query', 'info', 'warn'],
     datasources: { db: { url: Deno.env.get('DATA_PROXY_URL') as string } },
 })
